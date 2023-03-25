7d12934cc247f4f933c07f2a359f783373a9f2e5
Mostafa Med7t <mostafamedht98@gmail.com>
2023-03-25 14:36:57 +0200

    login user request

---
+++ app/Http/Requests/LoginUserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class LoginUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        if ($validator->fails()) {
+            throw new HttpResponseException(returnAuthenticationValidationError('Failed to login', $validator->errors()));
+        }
+    }
+}
