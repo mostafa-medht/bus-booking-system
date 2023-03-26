d601afd05e67d39a17a25c608d7c5ec7391a795d
Mostafa Med7t <mostafamedht98@gmail.com>
2023-03-26 21:22:13 +0200

    add station migrtion and seeders

---
+++ database/seeders/StationSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Station;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $stations = ['Cairo', 'Giza', 'AlFayyum', 'AlMinya', 'Asyut'];
+
+        foreach ($stations as $station) {
+            Station::create(['name' => $station]);
+        }
+    }
+}
