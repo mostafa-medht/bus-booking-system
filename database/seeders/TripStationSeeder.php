41f40763f09385306ea4f0c111b606360e6caa7d
Mostafa Med7t <mostafamedht98@gmail.com>
2023-03-26 21:34:23 +0200

    add trip station seeders

---
+++ database/seeders/TripStationSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Trip;
+use App\Models\TripStation;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TripStationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Get the Cairo to Asyut trip
+        $trip = Trip::where('start_station_id', 1)
+            ->where('end_station_id', 5)
+            ->first();
+
+        // Define the stations for the trip
+        $stations = [
+            ['station_id' => 3, 'stop_order' => 1],
+            ['station_id' => 4, 'stop_order' => 2],
+        ];
+
+        // Loop through each station and create a new TripStation record
+        foreach ($stations as $station) {
+            TripStation::create([
+                'trip_id' => $trip->id,
+                'station_id' => $station['station_id'],
+                'stop_order' => $station['stop_order'],
+            ]);
+        }
+    }
+}
