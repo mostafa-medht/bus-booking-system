40a348a6ac228bae0b3ddefb81d984aa4b3eb482
Mostafa Med7t <mostafamedht98@gmail.com>
2023-03-27 04:25:42 +0200

    add book seat function to controller

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,23 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BookSeatRequest;
 use App\Http\Requests\GetAvailableSeatsRequest;
-use App\Models\Seat;
-use App\Models\Trip;
-use Illuminate\Http\Request;
+use App\Services\BookingServices;
+use Illuminate\Http\Response;
 
 class BookingController extends Controller
 {
+
+    private BookingServices $bookingServices;
+
+    public function __construct(BookingServices $bookingServices)
+    {
+        $this->bookingServices = $bookingServices;
+    }
+
     public function getAvailableSeats(GetAvailableSeatsRequest $request)
     {
-
-        $tripId = $request->input('trip_id');
-        $tripSeats = Trip::with(['buses:id,bus_name', 'buses.seats:id,bus_id' => function($query){
-            $query->where('is_booked', '=', 0);
-        }])
-            ->where('id', $tripId)
-            ->firstOrFail();
+        return $this->bookingServices->getAvailableSeats($request->validated());
 
         return response()->json(['seats' => $tripSeats]);
+    }
+
+    public function bookSeat(BookSeatRequest $request)
+    {
+        $bookSeat = $this->bookingServices->bookSeat($request->validated());
+
+        if ($bookSeat['status'])
+            return Response($bookSeat, 200);
+
+        return Response($bookSeat, 422);
     }
 }
