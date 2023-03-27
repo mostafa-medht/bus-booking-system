b522b4c5330fc9237c757dec8c68d0f71777e4e9
Mostafa Med7t <mostafamedht98@gmail.com>
2023-03-27 04:26:01 +0200

    add book a seat request class

---
+++ app/Http/Requests/BookSeatRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BookSeatRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'seat_id' => 'required|integer|exists:seats,id',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        if ($validator->fails()) {
+            throw new HttpResponseException(returnCustomValidationError('please review the following errors',
+                $validator->errors()));
+        }
+    }
+}
